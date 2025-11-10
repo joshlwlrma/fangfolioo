742f75fe0e139f8d8f4b83db523f218d446cd232
joshlwlrma <mjmfangonilo@tip.edu.ph>
2025-11-10 10:47:32 +0800

    Revert "fixin"
    This reverts commit 2262ee5302f58cb405f00dd8f9c510cb851ceff4.

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,50 +1,30 @@
 import React from 'react';
-import './About.css'; // <-- Imports all styles
+import './About.css'; 
 
 function About() {
   return (
-    // This wrapper holds BOTH of your cards
-    <div className="about-page-wrapper">
-    
-      {/* --- CARD 1: Your 3D Flip Card (FangFolio ID) --- */}
-      <div className="card">
-        <img 
-          src={process.env.PUBLIC_URL + "/fangfolioid.png"} 
-          alt="About Me" 
-          className="card__icon" 
-        />
-        <div className="card__content">
-          <p className="card__title">About Me</p>
-          <p className="card__description">
-            Currently a 3nd Year Student at Technological Institute of the
-            Philippines, He is a student with interests in coding and has learned
-            languages like C++, Python, HTML, CSS, JavaScript, Java and is
-            currently learning the framework of Laravel and the React library. He
-            also has a passion for Graphic Designing and has taken a liking in
-            Web Development. Besides this, He has also grown fond of Networking,
-            Database Management, and Cybersecurity.
-            <br />
-            <br />
-            Other hobbies and interests include playing Video Games such as
-            Genshin Impact, Valorant, Mobile Legends and listening to Music and
-            watching Dramas.
-          </p>
-        </div>
+    <div className="card">
+
+<img src={process.env.PUBLIC_URL + "/fangfolioid.png"} alt="About Me" className="card__icon" />      
+      <div className="card__content">
+        <p className="card__title">About Me</p>
+        <p className="card__description">
+Currently a 3nd Year Student at Technological Institute of the Philippines, He is a student with interests in coding and has learned languages like C++, Python, HTML, CSS, JavaScript, Java and is currently learning the framework of Laravel and the React library. He also has a passion for Graphic Designing and has taken a liking in Web Development. Besides this, He has also grown fond of Networking, Database Management, and Cybersecurity.
+<br></br>
+<br></br>Other hobbies and interests include playing Video Games such as Genshin Impact, Valorant, Mobile Legends and listening to Music and watching Dramas.
+        </p>
       </div>
 
-      {/* --- CARD 2: Your New Stack Card (The Cat) --- */}
-      <div className="stack">
-        <div className="stack-card">
-          <div className="stack-image-container">
+<div className="stackcat">
+        <div className="cardcat">
+          <div className="imgcat">
             <img 
-              src={process.env.PUBLIC_URL + "/images/fangcats.png"} 
-              alt="Gabgab" 
-              className="stack-image"
+              src={process.env.PUBLIC_URL + "/fangcats.png"} 
             />
-          </div>
-          {/* This is the new title for your cat card */}
-        </div>
-      </div>
+
+    </div>
+  </div>
+</div>
 
     </div>
   );
